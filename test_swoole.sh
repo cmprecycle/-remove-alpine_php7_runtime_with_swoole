748298fe76abb2e95c516dfdcea35fa830ac3ecf
Wanjo Chan <wanjochan@gmail.com>
2016-03-10 14:58:02 +0800

    small bug fix

--- test_swoole.sh
+++ test_swoole.sh
@@ -1,1 +1,5 @@
-docker run -p 8080:9501 -v /home/wjc/cmptech/alpine_php7_runtime_with_swoole:/root/ cmptech/alpine_php7_runtime_with_swoole php /root/test_swoole.php
+dd=$(cd `dirname $0`; pwd)
+dt=`date +%Y%m%d%H%M%S`
+echo $dt
+
+docker run -p 8080:9501 -v ${dd}:/root/ cmptech/alpine_php7_runtime_with_swoole php /root/test_swoole.php
