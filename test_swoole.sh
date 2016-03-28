84c2685db4500b2cfe46c9afc1c6595ed1cc863c
Wanjo Chan <wanjochan@gmail.com>
2016-03-28 18:34:09 +0800

    add siege to test

--- test_swoole.sh
+++ test_swoole.sh
@@ -2,4 +2,7 @@ dd=$(cd `dirname $0`; pwd)
 dt=`date +%Y%m%d%H%M%S`
 echo $dt
 
-docker run -p 8080:9501 -v ${dd}:/root/ cmptech/alpine_php7_runtime_with_swoole php /root/test_swoole.php
+docker run -p 8080:9501 -v ${dd}:/root/ cmptech/alpine_php7_runtime_with_swoole php /root/test_swoole.php &
+sleep 2
+siege -c 10 -b -q -t 10s http://192.168.99.100:8080/
+
